e86e4931b84dee6b1425bd9b830462b1ddc9418c
santiagorepositorio <santiago.boris.q.a@gmail.com>
2024-03-01 19:05:01 -0400

    cambio listo

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -47,8 +47,8 @@
 @isset($actionText)
 <x-slot:subcopy>
 @lang(
-    "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-    'into your web browser:',
+    "Si tienes problemas para hacer clic en el botón \":actionText\" button, copia y pega la URL below\n".
+    'a continuación en tu navegador web:',
     [
         'actionText' => $actionText,
     ]
